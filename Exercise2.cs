6e371007091b3249c0a0064a214ef822d503e543
manouti1 <manouti1988@gmail.com>
2018-11-25 15:39:03 +0200

    feat: exercise 2

--- Exercise2.cs
+++ Exercise2.cs
@@ -11,9 +11,13 @@ namespace netcore_internship
                 var arr = new char[]{ 'z','q','a','u','w','r','c','f'};
                 // Your output should be: a c f q r u w z
 
-
-                
+                Array.Sort(arr);
+                for(int i = 0; i < arr.Length; i++)
+                {
+                   Console.Write( arr[i]+" ");
+                }
                 Console.WriteLine();
+                
             }
     }
 }
