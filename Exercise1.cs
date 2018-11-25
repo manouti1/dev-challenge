8578578dd86b9b170de53ea91058fda0b17fd0f6
manouti1 <manouti1988@gmail.com>
2018-11-25 15:31:45 +0200

    feat: exercise 1

--- Exercise1.cs
+++ Exercise1.cs
@@ -11,8 +11,29 @@ namespace netcore_internship
                 // Only output the occurence for a value once.
                 var arr = new int[]{ 6,6,9,8,5,4,7,8,5,3,2,3,2,3,6,5,8,5,8,6,9,6,9,9,5,8,7,4,7,8,4,5,9,5,2,6,5,8,7,1};
 
+              
                 // Your output should look like: 1=1 2=3 3=3 4=3 5=8 6=6 7=4 8=7 9=5
-                Console.WriteLine();
+                IDictionary<int, int> map = new Dictionary<int, int>();
+                foreach (int key in arr)
+                {
+                    if (map.ContainsKey(key))
+                    {
+                        int occurrence = map[key];
+                        occurrence++;
+                        map[key] = occurrence;
+                    }
+                    else
+                    {
+                        map[key] = 1;
+                    }
+                }
+
+                foreach (int key in map.Keys)
+                {
+                    int occurrence = map[key];
+                    Console.WriteLine(key + " = " + occurrence );
+                }
+
             }
     }
 }
